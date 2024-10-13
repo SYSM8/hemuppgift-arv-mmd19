f69e13f8131be53c94d8f9147d8703b9a34950ad
SofiaYe <SofiaYe@LAPTOP-8J4PDIHM>
2024-10-14 00:20:41 +0200

    Lagt till logiken för ComputerPlayer att slumpmässigt ta stickor baserat på brädans status och förbättring för hantering av slumpmässig stickplockning.

---
+++ Hemuppgift_Arv_Temp/Game/ComputerPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    public class ComputerPlayer : Player
+    {
+        Random random = new Random(); //Skapar ett random obj för att generera slumpmässiga tal
+
+        //Konstruktor
+        public ComputerPlayer(string userId) : base(userId)
+        {
+
+        }
+
+        //Metod
+        public override int TakePins(Board board)
+        {
+            //Slumpmässigt väljer 1 eller två stickor.
+            int NoPins = board.NoPins;
+            int taken;
+
+            if(NoPins == 1)
+            {
+                taken = 1;
+                Console.WriteLine($"Datorn tar {taken} sticka");
+            }
+           
+            else
+            {
+                taken=random.Next(1, Math.Min(3,NoPins+1));
+                Console.WriteLine($"Datorn tar {taken} sticka");
+            }
+
+            board.TakePins(taken);
+            return NoPins; //Returnerar det antal stickor som togs
+        }
+
+
+
+    }
+}
