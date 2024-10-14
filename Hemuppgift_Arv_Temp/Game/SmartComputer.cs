2c94186e37098f62fa6817b67ef4745c081d5133
SofiaYe <SofiaYe@LAPTOP-8J4PDIHM>
2024-10-15 00:05:36 +0200

    Lagt till SmartComputer-klassen med logik  för att välja stickor baserat på återstående antal stickor på brädet, samt fallback till slumpmässigt val när stickor är delbara med 3

---
+++ Hemuppgift_Arv_Temp/Game/SmartComputer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    public class SmartComputer : Player
+    {
+        //Konstruktor
+        public SmartComputer(string userId) : base(userId) 
+        { 
+
+        }
+
+        //Metod
+        public override int TakePins(Board board)
+        {
+            int pinCount = board.GetNoPins(); //Hämtning av variabel för beräkningar
+            int pinTake;
+
+            if (pinCount % 3 == 0)
+            {
+                Random random = new Random();
+                pinTake = random.Next(1, 3);
+            }
+            else
+            {
+                pinTake = pinCount % 3;
+            }
+
+            return pinTake;
+        }
+    }
+
+}
